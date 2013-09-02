3891a8666d0e9506139f9939acbfc66964079619
Myles Grant <myles@mylesgrant.com>
2013-09-02 15:11:24 -0700

    Towards json decoding and error handling

--- client.go
+++ client.go
@@ -1,13 +1,16 @@
 package rdio
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 const (
-	RDIO_API_ENDPOINT   = "http://api.rdio.com/1"
+	RDIO_API_ENDPOINT   = "http://api.rdio.com/1/"
 	RDIO_OAUTH_ENDPOINT = "http://api.rdio.com/oauth"
 )
 
@@ -17,26 +20,52 @@ type Client struct {
 	Token          string
 }
 
-func (c *Client) Call(method string, params map[string][]string) []byte {
+func (c *Client) Call(method string, params map[string][]string) interface{} {
 	params["method"] = []string{method}
-	// TODO: JSON decode
-	return c.SignedPost(RDIO_API_ENDPOINT, params)
+	body, err := c.SignedPost(RDIO_API_ENDPOINT, params)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse into json
+	var f interface{}
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return f
 }
 
-func (c *Client) SignedPost(url string, params map[string][]string) []byte {
+func (c *Client) SignedPost(url string, params map[string][]string) ([]byte, error) {
 	// Make call
 	resp, err := http.PostForm(url, params)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	// Make sure we close the body stream no matter what
+	defer resp.Body.Close()
+
+	// Check status code
+	switch resp.StatusCode {
+	default:
+		return nil, fmt.Errorf("Unknown status code: %d", resp.StatusCode)
+	case 403:
+		return nil, errors.New("Developer Inactive")
+	case 200:
+
 	}
 
 	// Read body
-	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
+	str := string(body)
+	fmt.Println(str)
+
 	// Return
-	return body
+	return body, nil
 }
