d0a82d2e0bf3b462a9f9190dfb2a03176c759e68
Myles Grant <myles@mylesgrant.com>
2013-09-03 21:38:17 -0700

    More work on signing oauth requests

--- client.go
+++ client.go
@@ -5,8 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"time"
 )
 
 const (
@@ -18,28 +22,33 @@ type Client struct {
 	ConsumerKey    string
 	ConsumerSecret string
 	Token          string
+	TokenSecret    string
 }
 
-func (c *Client) Call(method string, params map[string][]string) interface{} {
+func (c *Client) Call(method string, params url.Values) (interface{}, error) {
 	params["method"] = []string{method}
 	body, err := c.SignedPost(RDIO_API_ENDPOINT, params)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	// parse into json
 	var f interface{}
 	err = json.Unmarshal(body, &f)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	return f
+	return f, nil
 }
 
-func (c *Client) SignedPost(url string, params map[string][]string) ([]byte, error) {
+func (c *Client) SignedPost(postUrl string, params url.Values) ([]byte, error) {
+	// Sign the params
+	auth := c.Sign(postUrl, params)
+	fmt.Println(auth)
+
 	// Make call
-	resp, err := http.PostForm(url, params)
+	resp, err := http.PostForm(postUrl, params)
 	if err != nil {
 		return nil, err
 	}
@@ -47,25 +56,89 @@ func (c *Client) SignedPost(url string, params map[string][]string) ([]byte, err
 	// Make sure we close the body stream no matter what
 	defer resp.Body.Close()
 
-	// Check status code
-	switch resp.StatusCode {
-	default:
-		return nil, fmt.Errorf("Unknown status code: %d", resp.StatusCode)
-	case 403:
-		return nil, errors.New("Developer Inactive")
-	case 200:
-
-	}
-
 	// Read body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	str := string(body)
-	fmt.Println(str)
+	// Check status code
+	switch resp.StatusCode {
+	default:
+		str := string(body)
+		return nil, fmt.Errorf("Unknown status code: %d, %s", resp.StatusCode, str)
+	case 400:
+		return nil, errors.New("Bad Request")
+	case 403:
+		return nil, errors.New("Developer Inactive")
+	case 200:
+
+	}
 
 	// Return
+	return body, nil
+}
+
+func (c *Client) Sign(signUrl string, params url.Values) string {
+	params["oauth_version"] = []string{"1.0"}
+	params["oauth_timestamp"] = []string{strconv.FormatInt(time.Now().Unix(), 10)}
+	params["oauth_nonce"] = []string{strconv.FormatInt(rand.Int63(), 10)}
+	params["oauth_signature_method"] = []string{"HMAC-SHA1"}
+	params["oauth_consumer_key"] = []string{c.ConsumerKey}
+
+	// the consumer secret is the first half of the HMAC-SHA1 key
+	hmac_key := c.ConsumerSecret + "&"
+
+	if c.Token != "" {
+		// include a token in params
+		params["oauth_token"] = []string{c.Token}
+		// and the token secret in the HMAC-SHA1 key
+		hmac_key += c.TokenSecret
+	}
+
+	// sort the params by key
+	var keys []string
+	for k := range params {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	sorted := url.Values{}
+	for _, k := range keys {
+		sorted.Add(k, params.Get(k))
+	}
+
+	// build the signature base string
+	signatureBaseString := "POST&" + url.QueryEscape(signUrl) + "&" + sorted.Encode()
+	fmt.Println(signatureBaseString)
+
+	// Calculate HMAC-SHA1
+
+	return "OAuth "
+}
+
+func (c *Client) StartAuth() ([]byte, error) {
+	params := url.Values{
+		"oauth_callback": []string{"oob"},
+	}
+
+	body, err := c.SignedPost("http://api.rdio.com/oauth/request_token", params)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func (c *Client) CompleteAuth(verifier string) ([]byte, error) {
+	params := url.Values{
+		"oauth_verifier": []string{"verifier"},
+	}
+
+	body, err := c.SignedPost("http://api.rdio.com/oauth/access_token", params)
+	if err != nil {
+		return nil, err
+	}
+
 	return body, nil
 }
--- client_test.go
+++ client_test.go
@@ -2,8 +2,37 @@ package rdio
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
+
+func TestStartAuth(t *testing.T) {
+	c := &Client{
+		ConsumerKey:    "c9jx7x67bkmqm6ygnjwmbubd",
+		ConsumerSecret: "s53DhwNxuq",
+	}
+
+	body, err := c.StartAuth()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(body)
+}
+
+func TestCompleteAuth(t *testing.T) {
+	c := &Client{
+		ConsumerKey:    "c9jx7x67bkmqm6ygnjwmbubd",
+		ConsumerSecret: "s53DhwNxuq",
+	}
+
+	body, err := c.CompleteAuth("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(body)
+}
 
 func TestClient(t *testing.T) {
 	c := &Client{
@@ -12,6 +41,10 @@ func TestClient(t *testing.T) {
 	}
 
 	params := make(map[string][]string)
-	body := c.Call("getNewReleases", params)
+	body, err := c.Call("getNewReleases", params)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(body)
 }
