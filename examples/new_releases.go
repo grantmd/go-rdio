9f40b2942e6428ea641a6385574b5372dc70acb7
Myles Grant <myles@mylesgrant.com>
2013-09-04 23:12:36 -0700

    Towards allowing you to skip the auth steps in the example

--- examples/new_releases.go
+++ examples/new_releases.go
@@ -13,29 +13,37 @@ func main() {
 	c := &rdio.Client{
 		ConsumerKey:    config.ConsumerKey,
 		ConsumerSecret: config.ConsumerSecret,
+		Token:          config.Token,
+		TokenSecret:    config.TokenSecret,
 	}
 
-	// Start auth in order to redirect the user to approve our app
-	auth, err := c.StartAuth()
-	if err != nil {
-		log.Fatal(err)
-	}
+	if c.Token == "" {
 
-	// Tell the user what to do and wait for their PIN
-	fmt.Printf("Authorize this application at: %s?oauth_token=%s\n", auth.Get("login_url"), auth.Get("oauth_token"))
-	fmt.Print("Enter the PIN / OAuth verifier: ")
-	bio := bufio.NewReader(os.Stdin)
+		// Start auth in order to redirect the user to approve our app
+		auth, err := c.StartAuth()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	verifier, _, err := bio.ReadLine()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println()
+		// Tell the user what to do and wait for their PIN
+		fmt.Printf("Authorize this application at: %s?oauth_token=%s\n", auth.Get("login_url"), auth.Get("oauth_token"))
+		fmt.Print("Enter the PIN / OAuth verifier: ")
+		bio := bufio.NewReader(os.Stdin)
 
-	// Check their PIN and complete auth so we can make calls
-	auth, err = c.CompleteAuth(string(verifier))
-	if err != nil {
-		log.Fatal(err)
+		verifier, _, err := bio.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println()
+
+		// Check their PIN and complete auth so we can make calls
+		auth, err = c.CompleteAuth(string(verifier))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("New token: %s\n", auth.Get("oauth_token"))
+		fmt.Printf("New secret: %s\n", auth.Get("oauth_token_secret"))
 	}
 
 	// Make our first call
