9163e33f0dc4c645a7594536e0c49c0a5cca28c8
Myles Grant <myles@mylesgrant.com>
2013-09-20 19:34:34 -0700

    Starting on method tests

---
+++ methods_playback_test.go
@@ -0,0 +1,40 @@
+package rdio
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetPlaybackToken(t *testing.T) {
+	c := &Client{
+		ConsumerKey:    os.Getenv("RDIO_API_KEY"),
+		ConsumerSecret: os.Getenv("RDIO_API_SECRET"),
+		Token:          os.Getenv("RDIO_API_TOKEN"),
+		TokenSecret:    os.Getenv("RDIO_API_TOKEN_SECRET"),
+	}
+
+	if c.ConsumerKey == "" {
+		t.Error("Rdio api key is missing (should be in the RDIO_API_KEY environment variable)")
+	}
+
+	if c.ConsumerSecret == "" {
+		t.Error("Rdio api secret is missing (should be in the RDIO_API_SECRET environment variable)")
+	}
+
+	if c.Token == "" {
+		t.Error("Rdio api user token is missing (should be in the RDIO_API_TOKEN environment variable)")
+	}
+
+	if c.TokenSecret == "" {
+		t.Error("Rdio api user secret is missing (should be in the RDIO_API_TOKEN_SECRET environment variable)")
+	}
+
+	token, err := c.GetPlaybackToken()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if token == "" {
+		t.Error("Token is empty")
+	}
+}
